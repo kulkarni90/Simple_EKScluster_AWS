489865545a492da0d88575fc4a5d516a094609e1
phaneendra kulkarni <68918128+kulkarni90@users.noreply.github.com>
2020-12-11 11:55:44 +0530

    Output and values.

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "aws_vpc_id" {
+  value = "aws_vpc.k8-vpc.id"
+}
+
+output "aws_subnet_subnet_1" {
+  value = "aws_subnet.k8-subnet_1.id"
+}
+
+output "aws_subnet_subnet_2" {
+  value = "aws_subnet.k8-subnet_2.id"
+}
